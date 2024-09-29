7942eb4feecc13b794d42755a7b514b892d26f1a
Jean Paul <jeanpaulwebb@gmail.com>
2024-09-29 19:51:50 -0300

    :tada: Creating initial project with spring initialize

---
+++ src/main/java/com/develcode/checkout_ms_java/CheckoutMsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.develcode.checkout_ms_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutMsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutMsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develcode/checkout_ms_java/CheckoutMsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develcode.checkout_ms_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutMsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
